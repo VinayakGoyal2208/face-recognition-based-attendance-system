2304250669097a8565fcb055ae7f59a1782bac09
VinayakGoyal2208 <vinayakgoyal2004@gmail.com>
2024-07-14 12:25:38 +0530

    Add files via upload

---
+++ RealTimeDatabase/AddDatatoDatabase.py
@@ -0,0 +1,37 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://database-2e44e-default-rtdb.firebaseio.com/"
+})
+
+
+ref = db.reference('Students')
+
+data = {
+    "1234":
+        {
+            "name": "eleza",
+            "major": "course",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "45667":
+        {
+            "name": "elon",
+            "major": "course",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
